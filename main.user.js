06ec858c22897627cbd23ecbbdc14048b5b25c3e
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-12-09 16:08:37 +0200

    update

--- main.user.js
+++ main.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Mega AI PG
 // @namespace    http://tampermonkey.net/
-// @version      1.0.6
-// @description  MAIPG v.1.0.6
+// @version      1.0.7
+// @description  MAIPG v.1.0.7
 // @author       DAKO23
 // @match        https://megasys.megaparts.bg/megasys/product*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=chatgpt.com
