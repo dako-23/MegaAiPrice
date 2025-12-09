06398d2a5c5608ab9dbb7bf9142e0ded5660622f
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-12-09 16:01:13 +0200

    test update

--- main.user.js
+++ main.user.js
@@ -1,25 +1,28 @@
 // ==UserScript==
-// @name         Megaparts Auto Pricing Tool
+// @name         Mega AI PG
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Автоматично генериране и потвърждаване на цени в Megasys
-// @author       You
+// @version      1.0.5
+// @description  MAIPG v.1.0.5
+// @author       DAKO23
 // @match        https://megasys.megaparts.bg/megasys/product*
-// @run-at       document-end
-//
-// --- Модулите ---
-// @require      https://raw.githubusercontent.com/YOUR_NAME/megasys-automation/main/src/utils.js
-// @require      https://raw.githubusercontent.com/YOUR_NAME/megasys-automation/main/src/fillPrices.js
-// @require      https://raw.githubusercontent.com/YOUR_NAME/megasys-automation/main/src/confirmPrices.js
-// @require      https://raw.githubusercontent.com/YOUR_NAME/megasys-automation/main/src/buttons.js
-//
-// --- CSS ---
-// @resource     CUSTOMCSS https://raw.githubusercontent.com/YOUR_NAME/megasys-automation/main/src/styles.css
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=chatgpt.com
+
+// @require      https://raw.githubusercontent.com/dako-23/MegaAiPrice/main/src/utils.js
+// @require      https://raw.githubusercontent.com/dako-23/MegaAiPrice/main/src/fillPrices.js
+// @require      https://raw.githubusercontent.com/dako-23/MegaAiPrice/main/src/confirmPrices.js
+// @require      https://raw.githubusercontent.com/dako-23/MegaAiPrice/main/src/buttons/buttons.js
+
+// @resource     CUSTOMCSS https://raw.githubusercontent.com/dako-23/MegaAiPrice/main/src/buttons/styles.css
+
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
+
+// @run-at       document-end
 // ==/UserScript==
 
 (function () {
+    'use strict';
+
     GM_addStyle(GM_getResourceText("CUSTOMCSS"));
 
     initButtons();
